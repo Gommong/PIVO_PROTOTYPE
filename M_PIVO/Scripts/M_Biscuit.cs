1394675f2584cd6921644bef0f6292683b5d8a93
김헌주 <dkshqpfm@naver.com>
2018-11-22 23:08:03 +0900

    애옹 스테이지 배치끗 비스킷 냠냠

---
+++ M_PIVO/Scripts/M_Biscuit.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class M_Biscuit : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("비스킷돼라아");
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+       // if(other.tag  == "Player")
+       // {
+            Debug.Log("비스킷 뇸뇸");
+       // }
+    }
+}
